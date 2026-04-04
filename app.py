6ad6351d2e9f017c84d38a603620fb5bb89e025d
Vimal-Babu <Vimalpappu999@gmail.com>
2026-04-04 09:06:07 +0530

    fix manuvel core header

--- app.py
+++ app.py
@@ -1,50 +1,44 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from transcriber import transcribe_audio
 import os
 from werkzeug.utils import secure_filename
-from flask_cors import CORS
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER,exist_ok=True)
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": [
-    "https://enchanting-cassata-963cc5.netlify.app",
-    "http://localhost:3000"
-]}})
-
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "https://enchanting-cassata-963cc5.netlify.app"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    return response
 
 @app.route("/")
 def home():
     return "Backend is live!"
 
-@app.route("/transcribe", methods = ["POST","OPTIONS"])
+@app.route("/transcribe", methods=["POST", "OPTIONS"])
 def upload_file():
     if request.method == "OPTIONS":
-        return '',204
-    
+        return '', 204
+
     if "file" not in request.files:
-        return jsonify({"error":"No file uploaded"}), 400
-    
+        return jsonify({"error": "No file uploaded"}), 400
+
     file = request.files["file"]
     filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config["UPLOAD_FOLDER"],filename)
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     file.save(file_path)
-    
+
     try:
         transcription = transcribe_audio(file_path)
-        return jsonify({"transcription": transcription })
-    except Exception as e :
-        return jsonify({"error":str(e)}), 500
-    
-
+        return jsonify({"transcription": transcription})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    # app.run(debug=False)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
-    
